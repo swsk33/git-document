e3f070135c1bdf4acf762e59fc1143f8b7f49b96
守望时空33 <yanhanhui2001a@163.com>
2022-06-22 20:54:12 +0800

    修复乱码问题

--- server-springboot/src/main/java/com/gitee/swsk33/gitdocument/service/impl/EmailServiceImpl.java
+++ server-springboot/src/main/java/com/gitee/swsk33/gitdocument/service/impl/EmailServiceImpl.java
@@ -6,6 +6,7 @@ import com.gitee.swsk33.gitdocument.dataobject.User;
 import com.gitee.swsk33.gitdocument.model.ErrorReport;
 import com.gitee.swsk33.gitdocument.service.EmailService;
 import com.gitee.swsk33.readandwrite.param.NewLineCharacter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 public class EmailServiceImpl implements EmailService {
 
@@ -56,19 +58,20 @@ public class EmailServiceImpl implements EmailService {
 		StringBuilder content = new StringBuilder();
 		// 内容头
 		content.append("收到错误报告！").append(NewLineCharacter.defaultNewLineChar);
-		content.append("报告名：").append(errorReport.getName()).append(NewLineCharacter.defaultNewLineChar);
-		content.append("报告正文：").append(errorReport.getContent()).append(NewLineCharacter.defaultNewLineChar).append(NewLineCharacter.defaultNewLineChar);
+		content.append("- 报告名：").append(errorReport.getName()).append(NewLineCharacter.defaultNewLineChar);
+		content.append("- 报告正文：").append(errorReport.getContent()).append(NewLineCharacter.defaultNewLineChar).append(NewLineCharacter.defaultNewLineChar);
 		// 文章信息
 		content.append("错误涉及文章信息：").append(NewLineCharacter.defaultNewLineChar);
 		content.append("- 文章名：").append(errorReport.getArticle().getAnthology().getName()).append(" - ").append(errorReport.getArticle().getFilePath());
 		content.append(NewLineCharacter.defaultNewLineChar).append(NewLineCharacter.defaultNewLineChar);
 		// 报告人信息
 		content.append("来自用户：").append(NewLineCharacter.defaultNewLineChar);
-		content.append("用户昵称：").append(errorReport.getUser().getNickname()).append(NewLineCharacter.defaultNewLineChar);
-		content.append("用户邮箱：").append(errorReport.getUser().getEmail());
+		content.append("- 用户昵称：").append(errorReport.getUser().getNickname()).append(NewLineCharacter.defaultNewLineChar);
+		content.append("- 用户邮箱：").append(errorReport.getUser().getEmail());
 		List<User> admins = userDAO.getAllAdmin();
 		for (User user : admins) {
 			sendNotifyMail(user.getEmail(), "GitDocument - " + configProperties.getOrgName() + " - 错误报告", content.toString());
+			log.info("已向管理员：" + user.getNickname() + " 发送了错误报告通知邮件！");
 		}
 	}
 
