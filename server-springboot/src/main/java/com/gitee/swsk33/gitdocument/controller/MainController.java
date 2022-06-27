c1128a7fff9cfdec0d8a3e5c20e5be46a3ff754b
守望时空33 <yanhanhui2001a@163.com>
2022-06-27 22:06:50 +0800

    调整后端controller以适配vue router

--- server-springboot/src/main/java/com/gitee/swsk33/gitdocument/controller/MainController.java
+++ server-springboot/src/main/java/com/gitee/swsk33/gitdocument/controller/MainController.java
@@ -2,13 +2,20 @@ package com.gitee.swsk33.gitdocument.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
 	public String panel() {
-		return "/index.html";
+		return "/web/index.html";
+	}
+
+	// 重载带参方法以兼容vue router
+	@GetMapping("/{router}")
+	public String panel(@PathVariable("router") String router) {
+		return "/web/index.html";
 	}
 
 }
