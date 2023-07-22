e76ea73e44df5c5eedefc7a8126906ea6d13177a
守望时空33 <yanhanhui2001a@163.com>
2023-07-22 13:16:57 +0800

    已完成Dockerfile、docker-compose.yml和部署文档编写

--- dockerfile/build.sh
+++ dockerfile/build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-# 开发完成后的构建应用脚本，构建完成后才能构建Dockerfile
+version=$1
+
+if [ -z "$version" ]; then
+	echo 请指定版本号！
+	exit
+fi
+
 echo 清理上次构建...
-rm -rf ./*.tar
+rm -f ./*.tar ./*.jar
 echo 开始构建后端...
 cd ../server-springboot
 mvn clean package
-7z a -ttar ../dockerfile/springboot.tar ./target/git-document-*.jar ./external-resource '-xr!external-resource/avatar/user/' '-xr!external-resource/cover/custom/' '-xr!external-resource/background/custom.jpg' '-xr!external-resource/login-background/custom.jpg'
+cp -f ./target/git-document-*.jar ../dockerfile/git-document.jar
 echo 后端构建完成！
 echo 开始构建前端...
 cd ../web-vue-vite
 npm run build
 7z a -ttar ../dockerfile/vue.tar ./dist/*
 echo 前端构建完成！
+echo 构建Dockerfile...
+cd ../dockerfile
+docker build --network host --build-arg ALL_PROXY="http://127.0.0.1:7500" -f Dockerfile -t swsk33/git-document:$version .
+echo 创建latest标签...
+docker tag swsk33/git-document:$version swsk33/git-document
+echo 全部构建完成！
--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 /etc/init.d/ssh restart
 nginx
-sudo -u git java -jar \
--Dspring.datasource.url="jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/git_doc?serverTimezone=GMT%2B8" \
--Dspring.datasource.username=$MYSQL_USER \
--Dspring.datasource.password=$MYSQL_PASSWORD \
--Dspring.data.redis.host=$REDIS_HOST \
--Dspring.data.redis.port=$REDIS_PORT \
--Dspring.data.redis.password=$REDIS_PASSWORD \
--Dspring.rabbitmq.host=$RABBIT_MQ_HOST \
--Dspring.rabbitmq.port=$RABBIT_MQ_PORT \
--Dspring.rabbitmq.username=$RABBIT_MQ_USER \
--Dspring.rabbitmq.password=$RABBIT_MQ_PASSWORD \
--Dspring.mail.host=$EMAIL_SMTP_HOST \
--Dspring.mail.username=$EMAIL_USER \
--Dspring.mail.password=$EMAIL_PASSWORD \
-git-document-*.jar
+echo 延迟5秒以等待其它服务启动...
+sleep 5
+exec sudo -u git java -jar \
+	-Dspring.datasource.url="jdbc:postgresql://$POSTGRE_HOST:$POSTGRE_PORT/git_doc?TimeZone=Asia/Shanghai" \
+	-Dspring.datasource.username=$POSTGRE_USER \
+	-Dspring.datasource.password=$POSTGRE_PASSWORD \
+	-Dspring.data.redis.host=$REDIS_HOST \
+	-Dspring.data.redis.port=$REDIS_PORT \
+	-Dspring.data.redis.password=$REDIS_PASSWORD \
+	-Dspring.rabbitmq.host=$RABBIT_MQ_HOST \
+	-Dspring.rabbitmq.port=$RABBIT_MQ_PORT \
+	-Dspring.rabbitmq.username=$RABBIT_MQ_USER \
+	-Dspring.rabbitmq.password=$RABBIT_MQ_PASSWORD \
+	-Dio.github.swsk33.code-post.core.smtp-host=$EMAIL_SMTP_HOST \
+	-Dio.github.swsk33.code-post.core.email=$EMAIL_USER \
+	-Dio.github.swsk33.code-post.core.password=$EMAIL_PASSWORD \
+	-Dcom.gitee.swsk33.git-doc.host-port=$HOST_SSH_PORT \
+	git-document.jar
