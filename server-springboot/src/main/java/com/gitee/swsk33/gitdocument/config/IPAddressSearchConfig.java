24e76066631d3cbd4b335d8792c24d6f090accd7
依赖倒不置 <yanhanhui2001a@163.com>
2025-10-01 12:59:48 +0800

    升级依赖

--- server-springboot/src/main/java/com/gitee/swsk33/gitdocument/config/IPAddressSearchConfig.java
+++ server-springboot/src/main/java/com/gitee/swsk33/gitdocument/config/IPAddressSearchConfig.java
@@ -2,7 +2,9 @@ package com.gitee.swsk33.gitdocument.config;
 
 import cn.hutool.core.io.resource.ClassPathResource;
 import lombok.extern.slf4j.Slf4j;
+import org.lionsoul.ip2region.xdb.LongByteArray;
 import org.lionsoul.ip2region.xdb.Searcher;
+import org.lionsoul.ip2region.xdb.Version;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,7 +27,7 @@ public class IPAddressSearchConfig {
 		// 读取IP数据库文件
 		ClassPathResource resource = new ClassPathResource("ip2region.xdb");
 		try (InputStream stream = resource.getStream()) {
-			searcher = Searcher.newWithBuffer(stream.readAllBytes());
+			searcher = Searcher.newWithBuffer(Version.IPv4, new LongByteArray(stream.readAllBytes()));
 		} catch (Exception e) {
 			log.error("读取IP数据库文件出错！IP查询器初始化失败！");
 			log.error(e.getMessage());
